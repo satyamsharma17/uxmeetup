8f198086c2703eac4a914a80951156785df1c996
Satyam Sharma <satyamsharma1725@gmail.com>
2023-02-25 10:33:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+const downloadCharacterSheet = () => {
+  
+  const node = document.getElementById('id-card');
+  
+  html2canvas(node).then(canvas => {
+    // document.body.appendChild(canvas)
+    // var img    = canvas.toDataURL("image/png");
+    // document.write('<img src="'+img+'"/>');
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = canvas.toDataURL()
+    link.click();
+  });
+  
+};
+
+const bindInputToElement = (inputEl, elementEl) => {
+  inputEl.addEventListener('change', () => {
+    elementEl.textContent = inputEl.value;
+  });
+}
+
+document
+  .getElementById('download-button')
+  .addEventListener('click', downloadCharacterSheet);
+
+// Bind mugshot
+document
+    .getElementById('mugshot')
+    .addEventListener('change', function() {
+      if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = document.getElementById('id-card-mugshot');
+           img.src = e.target.result;
+        };       
+        FR.readAsDataURL( this.files[0] );
+      }
+    });
